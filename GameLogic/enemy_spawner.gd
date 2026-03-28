00f3308acd08924002a2e5d705e59e998007e8d2
Travis-A <arguellotravis@gmail.com>
2026-03-28 16:37:03 -0500

    added spawner

--- GameLogic/enemy_spawner.gd
+++ GameLogic/enemy_spawner.gd
@@ -1,27 +1,11 @@
-extends Node2D
+extends Node
 
 var enemy_scene = preload("res://Enemy/enemy.tscn")
-var margin = 100 # Pixels outside screen
 
-
+@export var spawn_locations : Array[Node2D]
 
 func spawn_enemy():
-	var viewport_rect = get_viewport_rect()
-	var spawn_pos = Vector2.ZERO
-	
-	# Randomly choose edge: 0=Top, 1=Bottom, 2=Left, 3=Right
-	var edge = randi() % 4
-	
-	match edge:
-		0: # Top
-			spawn_pos = Vector2(randf_range(0, viewport_rect.size.x), -margin)
-		1: # Bottom
-			spawn_pos = Vector2(randf_range(0, viewport_rect.size.x), viewport_rect.size.y + margin)
-		2: # Left
-			spawn_pos = Vector2(-margin, randf_range(0, viewport_rect.size.y))
-		3: # Right
-			spawn_pos = Vector2(viewport_rect.size.x + margin, randf_range(0, viewport_rect.size.y))
-			
+	var spawn_pos = spawn_locations.pick_random().global_position
 	var enemy = enemy_scene.instantiate()
 	enemy.global_position = spawn_pos
 	get_tree().current_scene.add_child(enemy)
